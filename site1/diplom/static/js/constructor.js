c516370636b8bbfa3b7483a8e3dea7b43ec3a296
ShapovalSergey <your_email@whatever.com>
2024-05-21 22:21:27 +0700

    Добавлено окно для создания блюда

--- site1/diplom/static/js/constructor.js
+++ site1/diplom/static/js/constructor.js
@@ -16,8 +16,16 @@ $(document).ready(function() {
     document.location.href=local[0]+"/"+local[1];
 }
 
+function showPopup()
+{
+    document.getElementsByClassName("add_cake_back")[0].setAttribute("style",'display:block');
+    document.getElementsByClassName("add_cake_window")[0].setAttribute("style",'display:block');
+    const p = document.getElementById("popup");
+    p.style.transform=`translateY(70%)`
+}
+function closePopup()
+{
+    document.getElementsByClassName("add_cake_back")[0].setAttribute("style",'display:none');
+    document.getElementsByClassName("add_cake_window")[0].setAttribute("style",'display:none');
+}
 
-window.addEventListener("load", (event) => {
-  const value = JSON.parse(document.getElementById('hello-data').textContent);
-  //alert(value);
-});
